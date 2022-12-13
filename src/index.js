7a2e5a820576c64e97ea7cb17f0671d8c18e9a37
ZhenyaIlchenko <108277664+ZhenyaIlchenko@users.noreply.github.com>
2022-12-13 14:35:59 +0200

    corrected index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './css/styles.css';
 import { fetchImages } from './js/fetch-images';
 import { renderGallery } from './js/render-gallery';
-// import { onScroll, onToTopBtn } from './js/scroll';
+import { onScroll, onToTopBtn } from './js/scroll';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
